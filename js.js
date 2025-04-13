5ad9bbd909ce4ddc8846de02ff1308d4b3b3baf1
morolo223 <rlucenko330@gmail.com>
2025-04-13 15:51:21 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+// $(function() {
+//     function colorChange() {
+//         let r = Math.floor(Math.random() * 255);
+//         let g = Math.floor(Math.random() * 255);
+//         let b = Math.floor(Math.random() * 255);
+
+//         $('a').css('color', `rgb(${r}, ${g}, ${b})`);
+//     }
+
+//     setInterval(colorChange, 700);
+// });
